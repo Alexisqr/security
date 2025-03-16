ac61a0f04be822544298f6b6d1a32c58080fcc9e
Alex <alexis.cv1824@gmail.com>
2025-03-16 14:49:58 +0200

    project first ex1

---
+++ src/main/java/edu/oleks/security/Item/AnimalRestController.java
@@ -0,0 +1,46 @@
+package edu.oleks.security.Item;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*
+@author   oleksandra
+@project   security
+@class  ItemRestController
+@version  1.0.0
+@since 19.02.2025 - 22.57
+*/
+@RestController
+@RequestMapping("/api/v1/animals")
+@AllArgsConstructor
+public class AnimalRestController {
+
+    @Autowired
+    private AnimalService animalService;
+
+    @GetMapping
+    public List<Animal> getItems() {
+        return animalService.getAll();
+    }
+    @GetMapping("/{id}")
+    public Animal getOneItem(@PathVariable String id) {
+        return animalService.getById(id);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        animalService.deleteById(id);
+    }
+
+    @PostMapping
+    public Animal create(@RequestBody Animal animal) {
+        return animalService.create(animal);
+    }
+
+    @PutMapping
+    public Animal update(@RequestBody Animal animal) {
+        return animalService.update(animal);
+    }
+}
---
+++ src/main/java/edu/oleks/security/Item/AnimalService.java
@@ -0,0 +1,53 @@
+package edu.oleks.security.Item;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+@author   oleksandra
+@project   security
+@class  ItemService
+@version  1.0.0
+@since 19.02.2025 - 22.54
+*/
+
+@Service
+public class AnimalService {
+    @Autowired
+    private AnimalRepository animalRepository;
+    private List<Animal> animals = new ArrayList<Animal>();
+
+    @PostConstruct
+    void init() {
+        animals.add(new Animal("1","name1",1,"M","description1","location","cat","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww",false,"1",true));
+        animals.add(new Animal("2","name2",1,"F","description2","location2","dog","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww",false,"2",false));
+        animals.add(new Animal("3","name3",1,"M","description3","location3","cat","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww",false,"1",true));
+
+        animalRepository.saveAll(animals);
+    }
+
+
+    public List<Animal> getAll() {
+        return animalRepository.findAll();
+    }
+
+    public Animal getById(String id) {
+        return animalRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(String id) {
+        animalRepository.deleteById(id);
+    }
+
+    public Animal create(Animal item) {
+        return animalRepository.save(item);
+    }
+
+    public Animal update(Animal item) {
+        return animalRepository.save(item);
+    }
+}
